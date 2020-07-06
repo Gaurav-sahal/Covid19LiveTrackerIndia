43135f1faa10cfb544f9e2d9a3fd56927573ff90
Gaurav Sahal <gaurav.sahal10@gmail.com>
2020-07-06 17:11:20 +0530

    Forth Commit: Marquee added & footer updated

--- script.js
+++ script.js
@@ -1,23 +1,24 @@
 
-
-$.getJSON("https://api.apify.com/v2/key-value-stores/toDWvRj1JpTXiM8FF/records/LATEST?disableRedirect=true",
-	function(data){
-		console.log(data);
+/* THIS FUNCTION STARTS AT THE TIME OF RELOAD OF PAGE */
+var mainUrl = "https://api.apify.com/v2/key-value-stores/toDWvRj1JpTXiM8FF/records/LATEST?disableRedirect=true";
+$.getJSON(mainUrl, function(data)
+	{
+		console.log(data);	//PRINTS OUT ALL THE JSON DATA IN THE CONSOLE
 
 		var scriptUpdatedTime = data.lastUpdatedAtApify;
-		console.log(scriptUpdatedTime);
-
+		//console.log(scriptUpdatedTime);
 		var scriptActiveCases = data.activeCases;
 		var scriptRecovered = data.recovered;
 		var scriptDeaths = data.deaths;
 		var scriptTotalCases = data.totalCases;
 		
-		var json = JSON.stringify(scriptUpdatedTime); 
+		var json = JSON.stringify(scriptUpdatedTime); //CONVERTS DATE/TIME INTO STRING FORMAT
 		var dateStr = JSON.parse(json);  
-		console.log(dateStr);         
+		//console.log(dateStr);         
 		var date = new Date(dateStr);
-		console.log(date);
+		console.log("Last Updated "+date);	//PRINTS LAST REFRESHED TIME IN THE CONSOLE
 
+		/* PUTTING THE VARIABLES DATA TO THE UPPER DETAIL SECTION */
 		$(".indexActiveCases").append(scriptActiveCases);
 		$(".indexRecovered").append(scriptRecovered);
 		$(".indexDeaths").append(scriptDeaths);
@@ -27,34 +28,37 @@ $.getJSON("https://api.apify.com/v2/key-value-stores/toDWvRj1JpTXiM8FF/records/L
 		
 );
 
-var scriptState;  // = "bihar";
+var scriptState;  // DECLARING THIS VARIABLE GLOBALLY
 
+/* THIS FUNCTION WILL BE CALLED WHEN CLICKED ON SEARCH BUTTON AFTER SELECTING A STATE */
 function searchRegion() {
 
+	/* BELOW 5 COMMANDS WILL ERASE THE PREVIOUS CONTAINED DATA */
 	$(".indexState").empty();
 	$(".indexStateDeceased").empty();
 	$(".indexStateRecovered").empty();
 	$(".indexStateTotalCases").empty();
 	$(".indexStateTotalInfected").empty();
 
+	/* BELOW COMMANDS WILL UNHIDE/HIDE PARTICULAR FIELDS */
 	document.getElementById("middle_detail").style.visibility="visible";	
 	document.getElementById("DistrictSearch").style.visibility="visible";
 	document.getElementById("lower_detail").style.visibility="hidden";
 	// document.getElementById("indexDistrict").reset();
 	document.getElementById('indexDistrict').value = '';
 
-	scriptState = document.getElementById("indexDropdownRegion").value;
-	//console.log(scState);
+	scriptState = document.getElementById("indexDropdownRegion").value;	//CHOOSEN VALUE OF STATE IS ASSIGNED TO THE GIVEN FIELD
+	console.log("CHOOSE A STATE: "+scriptState);	//PRINTS THE SELECTED STATE
 
-	$.getJSON("https://api.apify.com/v2/key-value-stores/toDWvRj1JpTXiM8FF/records/LATEST?disableRedirect=true",
-		function(data){
-	
-			console.log(scriptState);
 
+	/* BELOW JQUERY PUT THE RESPECTIVE STATE DATA TO THE MIDDLE DETAIL SECTION FIELD  */
+	$.getJSON(mainUrl, function(data)
+		{
 			var found = 0;
 			for (var i = 0; i < Object.keys(data.regionData).length; i++){
 				if (data.regionData[i].region == scriptState){
 	        		found = 1;
+	        		/* PUTTING THE VARIABLES DATA TO THE MIDDLE DETAIL SECTION */
 	        		$(".indexState").append("State: "+scriptState);
 					$(".indexStateDeceased").append("Deaths: "+data.regionData[i].deceased);
 					$(".indexStateRecovered").append("Recovered: "+data.regionData[i].recovered);
@@ -76,25 +80,24 @@ function searchRegion() {
 	dropdown.append('<option selected="true" disabled>Choose a District</option>');
 	dropdown.prop('selectedIndex', 0);
 
-	const url = 'https://api.covid19india.org/state_district_wise.json';
+	const url = 'https://api.covid19india.org/state_district_wise.json';	//URL OF POSTMAN API
 
-	// Populate dropdown with list of districts
+	/* BELOW JQUERY POPULATES DROPDOWN WITH LIST OF DISTRICTS */  
 	$.getJSON(url, function (data) {
+		console.log("AVAILABLE DISTRICTS");		//PRINTS AVAILABLE DISTRICTS OF SELECTED STATE
 	  $.each(data[scriptState].districtData, function (key) {
-	     dropdown.append($('<option></option>').attr('value', key).text(key));
-	    // if(key == scriptState){
-	    	console.log("District "+key);
-	    //	console.log("This is entry"+entry);
-	    // }
-	    // else{
-	    // 	console.log("Region not found!");
-	    // }
+
+	    dropdown.append($('<option></option>').attr('value', key).text(key));
+	    console.log(key);	//PRINTS OUT ALL THE DISTRICTS OF RESPECTIVE STATE
+
 	  })
 	});
 }
 
+/* THIS FUNCTION WILL BE CALLED WHEN CLICKED ON SEARCH BUTTON AFTER SELECTING A DISTRICT */
 function searchDistrict(){
 
+	/* BELOW 6 COMMAND ERASE THE PREVIOUS CONTAINED DATA OF GIVEN FIELDS OF LOWER DETAIL SECTION */
 	$(".indexDistrict").empty();
 	$(".indexDistrictConfirmed").empty();
 	$(".indexDistrictRecovered").empty();
@@ -104,21 +107,24 @@ function searchDistrict(){
 
 	var scriptDistrict = document.getElementById("indexDistrict").value;
 
-	$.getJSON("https://api.covid19india.org/state_district_wise.json",
-		function(data){
+	const url = 'https://api.covid19india.org/state_district_wise.json';	//URL OF POSTMAN API
 
+	$.getJSON(url, function(data)
+		{
 			// var scriptDistrict = document.getElementById("indexDistrict").value;
+			/* IF CHOOSEN STATE IS EQUAL TO FOUND STATE, THEN BELOW CODE EXECUTE */
 			if(data[scriptState].districtData[scriptDistrict]){
 
-				document.getElementById("lower_detail").style.visibility="visible";
+				document.getElementById("lower_detail").style.visibility="visible";		//UNHIDE LOWER DETAIL SECTION
 
-				console.log(scriptState);	
-				console.log(scriptDistrict);
+				console.log("SELECTED STATE: "+scriptState);	
+				console.log("SELECTED DISTRICT: "+scriptDistrict);
 				var districtActive = data[scriptState].districtData[scriptDistrict].active;
 				var districtConfirmed = data[scriptState].districtData[scriptDistrict].confirmed;
 				var districtDeceased = data[scriptState].districtData[scriptDistrict].deceased;
 				var districtRecovered = data[scriptState].districtData[scriptDistrict].recovered;
 
+				/* PUTTING THE VARIABLES DATA TO THE LOWER DETAIL SECTION */
 				$(".indexDistrict").append("District: "+scriptDistrict);
 				$(".indexDistrictConfirmed").append("Total Confirmed: "+districtConfirmed);
 				$(".indexDistrictRecovered").append("Recovered: "+districtRecovered);
@@ -127,16 +133,13 @@ function searchDistrict(){
 
 			}
 			else{
-				$(".indexDistrictErrorCase").append("District Not Found! Please check Spelling");
+				$(".indexDistrictErrorCase").append("District Not Found! Please check Spelling");	//PRINTS OUT IF SOMETHING GOES WRONG
 				document.getElementById("lower_detail").style.visibility="hidden";
 				console.log("District Not Found! Please check Spelling");
 			}
 		
 		}
 	);
-
-	
-
 }
 
 
